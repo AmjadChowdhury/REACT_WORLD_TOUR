691e5ae35b942b7e1cae3ed9c399df502a72c711
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-04-01 12:07:15 +0100

    handlevisitedcountries state set in parents

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,17 +4,30 @@ import Country from "../Country/Country";
 
 const Countries = () => {
     const [countries,setCountries] = useState([])
+    const [visitedCountries,setVisiteCountries] = useState([])
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+
+    const handleVisitedCountries = country => {
+        console.log('Add this')
+        console.log(country)
+    }
     return (
         <div>
             <h2>Countries : {countries.length}</h2>
+            <div>
+                <h5>Visited Countries</h5>
+                <ul>
+
+                </ul>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
                 {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                    countries.map(country => <Country key={country.cca3} country={country} handleVisitedCountries={handleVisitedCountries}></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
 
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountries}) => {
     console.log(country)
     const {name,flags,cca3} = country
     const [visited,setVisited] = useState(false)
     const handleVisited = () => {
         setVisited(!visited)
     }
+
+    console.log(handleVisitedCountries)
     return (
         <div className={`border-4 border-dotted rounded-lg p-5 ${visited ? 'bg-green-400' : 'bg-yellow-300'}`}>
             <p>Name : {name.common}</p>
